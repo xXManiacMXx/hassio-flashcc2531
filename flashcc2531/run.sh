dccec853825947d5334ed12e2d49c2d0d7413474
claudiu-mihai <mihai.ioan@gmail.com>
2022-09-19 14:21:59 +0300

    Update run.sh

--- flashcc2531/run.sh
+++ flashcc2531/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Startet"
 cd /flash_cc2531
-if ! ./cc_chipid | grep "ID = b524"; then echo "ChipID not found." && exit 1; fi
+#if ! ./cc_chipid | grep "ID = b524"; then echo "ChipID not found." && exit 1; fi
 
 echo "Downloading firmware"
 if ! wget https://github.com/Koenkk/Z-Stack-firmware/raw/master/coordinator/Z-Stack_3.x.0/bin/CC1352P2_CC2652P_launchpad_coordinator_20220219.zip; then echo "firmware not found" && exit 1; fi
