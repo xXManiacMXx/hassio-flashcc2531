fc6c0e8a14e2e2f9d6837356697e48c2a341f230
claudiu-mihai <mihai.ioan@gmail.com>
2022-09-19 14:15:41 +0300

    Update run.sh

--- flashcc2531/run.sh
+++ flashcc2531/run.sh
@@ -4,10 +4,10 @@ cd /flash_cc2531
 if ! ./cc_chipid | grep "ID = b524"; then echo "ChipID not found." && exit 1; fi
 
 echo "Downloading firmware"
-if ! wget https://github.com/claudiu-mihai/hassio-flashcc2531/raw/master/CC2531_DEFAULT_20201127.zip; then echo "firmware not found" && exit 1; fi
+if ! wget https://github.com/Koenkk/Z-Stack-firmware/raw/master/coordinator/Z-Stack_3.x.0/bin/CC1352P2_CC2652P_launchpad_coordinator_20220219.zip; then echo "firmware not found" && exit 1; fi
 
 echo "unziping"
-if ! 7z x CC2531_DEFAULT_20201127.zip; then echo "unzip failed" && exit 1; fi
+if ! 7z x CC1352P2_CC2652P_launchpad_coordinator_20220219.zip; then echo "unzip failed" && exit 1; fi
 
 echo "backup firmware"
 ./cc_read save.hex
@@ -17,6 +17,6 @@ echo "erase"
 ./cc_erase
 
 echo "flash firmware"
-./cc_write CC2531ZNP-Prod.hex
+./cc_write CC1352P2_CC2652P_launchpad_coordinator_20220219.hex
 
 echo "Finished"
