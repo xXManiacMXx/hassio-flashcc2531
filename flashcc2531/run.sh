7691bdcb487e23e380bc4df6133cf71902a392c0
xXManiacMXx <86500992+xXManiacMXx@users.noreply.github.com>
2024-01-22 19:10:59 +0100

    Update run.sh

--- flashcc2531/run.sh
+++ flashcc2531/run.sh
@@ -4,10 +4,10 @@ cd /flash_cc2531
 #if ! ./cc_chipid | grep "ID = b524"; then echo "ChipID not found." && exit 1; fi
 
 echo "Downloading firmware"
-if ! wget https://github.com/Koenkk/Z-Stack-firmware/raw/master/coordinator/Z-Stack_3.x.0/bin/CC1352P2_CC2652P_launchpad_coordinator_20220219.zip; then echo "firmware not found" && exit 1; fi
+if ! wget https://github.com/Koenkk/Z-Stack-firmware/raw/master/coordinator/Z-Stack_3.0.x/bin/CC2531_20190425.zip; then echo "firmware not found" && exit 1; fi
 
 echo "unziping"
-if ! 7z x CC1352P2_CC2652P_launchpad_coordinator_20220219.zip; then echo "unzip failed" && exit 1; fi
+if ! 7z x CC2531_20190425.zip; then echo "unzip failed" && exit 1; fi
 
 echo "backup firmware"
 ./cc_read save.hex
@@ -17,6 +17,6 @@ echo "erase"
 ./cc_erase
 
 echo "flash firmware"
-./cc_write CC1352P2_CC2652P_launchpad_coordinator_20220219.hex
+./cc_write CC2531_20190425.hex
 
 echo "Finished"
